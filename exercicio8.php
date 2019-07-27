9454e8498c4499909ce15069448dc01c24be5726
Eduardo Onofre <48128069+UnbreakableEdu@users.noreply.github.com>
2019-07-27 19:53:12 -0300

    Update exercicio8.php

--- exercicio8.php
+++ exercicio8.php
@@ -6,7 +6,7 @@
     
     
     print "\n Digite uma palavra ou frase: ";
-        $textoPedido = (string) trim(fgets(STDIN));
+        $textoPedido = (string) fgets(STDIN);
         $qtdLetras = quantidadeCaracteres($textoPedido);
         print "\n No texto digitado há um total de " . $qtdLetras . " caracteres. \n";
 ?>
